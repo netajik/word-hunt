8997620602e3f40a3d7e9d9c768bf1ff14ae6d36
netajik <kancharapunetaji@gmail.com>
2021-10-13 23:24:07 +0530

    intitial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { Container } from '@material-ui/core';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [word, setWord] = useState([]);
+  const [meanings, setmeanings] = useState([]);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+      );
+
+      setmeanings(data.data);
+    } catch (error) {
+      throw error;
+    }
+  }
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+  return (
+    <div 
+      className="App"
+      style={{ height: "100vh", backgroundColor: "#282c34", color: "white"}}
+    >
+      <Container
+        maxWidth="md"
+        style={{ display: "flex", flexDirection: "column", height: "100vh"}}
+      >
+        Dictionary
+      </Container>
+    </div>
+  );
+}
+
+export default App;
